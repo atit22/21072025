874543316fccb789d32b67c256456943821f6f11
unknown <atit22@gmail.com>
2025-07-21 00:28:01 +0530

    feature101

--- parent/module.tf
+++ parent/module.tf
@@ -3,6 +3,16 @@ source   = "../child"
 resource_group_name="RG-Atit2"
 location="East US"
 }
+module "rs-at1" {
+source   = "../child"
+resource_group_name="RG-Atit3"
+location="East US"
+}
+module "rs-at1" {
+source   = "../child"
+resource_group_name="RG-Atit3"
+location="East US"
+}
 module "storage" {
 source= "../child"  
 depends_on = [ module.rs-at1 ]
